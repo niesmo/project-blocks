ab48c01e9bebe3da665619b3cf49f9e7c5ade7a2
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2015-11-30 23:05:08 -0500

    init testing

---
+++ android/app/src/main/java/com/osu/cse/projectblocks/NumberAdder.java
@@ -0,0 +1,10 @@
+package com.osu.cse.projectblocks;
+
+/**
+ * Created by niesmo on 11/30/2015.
+ */
+public class NumberAdder {
+    public static int addNumbers(int a, int b){
+        return a+b;
+    }
+}
